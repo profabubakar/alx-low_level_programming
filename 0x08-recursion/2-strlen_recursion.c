c9a7f253319b9c4f159874b0ba3bce93af796c25
Iyanda Abubakar <iyandaabubakar@gmail.com>
2022-07-20 09:07:23 -0700

    1recur

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,16 +1,14 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * _strlen_recursion - size
- * @s: pointer to string params
- * Return: recursion
- */
-
+* _strlen_recursion -> function to get length of string
+* @s: string param
+* Return: length of string
+*/
 int _strlen_recursion(char *s)
-{
-	if (!*s)
-	{
+{	
+	if (*s == '\0')
 		return (0);
-	}
-	return (1 + _strlen_recursion(++s));
+	s++;
+	return (1 + _strlen_recursion(s));
 }
